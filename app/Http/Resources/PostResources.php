e422fdc7a62d977c657fa37c13314bccdbc6108f
May-Abdelghany <mayawaad622@gmail.com>
2021-09-01 13:27:05 +0200

    error writing

---
+++ app/Http/Resources/PostResources.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+namespace App\Http\Resources\postResources;
+use App\Models\post;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Request;
+class PostResources extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+               'title'=>$this->title,
+               'description'=>$this->description,
+        ];
+    }
+    public function show(Request $request ,post $post){
+        return new PostResources($post);
+}
+}
