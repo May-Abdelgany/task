87bc84d905c4de1cdc051e7e49136bc572b35f5d
May-Abdelghany <mayawaad622@gmail.com>
2021-09-01 10:15:25 +0200

    it is controller between model and route

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\PostRequest;
+use App\Http\Resources\PostResouces;
 use Illuminate\Http\Request;
 use App\Models\post;
 class PostsController extends Controller
@@ -10,5 +11,29 @@ class PostsController extends Controller
     {
         $posts=post::all();
         return $posts;
+    }
+
+    public function sub()
+    {
+        $posts=post::paginate(15);
+        return $posts;
+    }
+
+    public function show(Request $request ,Post $post)
+    {
+        $post=post::find($post);
+        return $post;
+    }
+
+    public function store(PostRequest $request)
+    {
+      $post=new post;
+        $post->title=$request->title;
+        $post->descreption=$request->dscreption;
+        $post->save();
+        return response([
+            'data' => new $post,
+        ]);
+
     }
 }
